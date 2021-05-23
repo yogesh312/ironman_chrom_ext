832d198360cd942897188fa8cf445d5ca52069dc
yogesh <etpr.yogesh@gmail.com>
2021-05-23 22:45:33 +0530

    script for ext

---
+++ index.js
@@ -0,0 +1,22 @@
+let searchButton = document.getElementById("searchButton")
+let searchBox = document.getElementById("searchBox")
+searchButton.addEventListener("click", function (event) { // When search button is clicked
+    event.preventDefault()
+
+    window.location.replace("https://www.google.com/search?q=" + searchBox.value) // Google search
+})
+const clock = document.getElementById('clock');
+
+const setClock = () => {
+  let time = new Date();
+  let hours = time.getHours();
+  let minutes = time.getMinutes();
+  let seconds = time.getSeconds();
+  if (hours < 10) hours = "0" + hours;
+  if (minutes < 10) minutes = "0" + minutes;
+  if (seconds < 10) seconds = "0" + seconds;
+  clock.innerText = document.title = hours + ":" + minutes + ":" + seconds
+}
+
+setClock();
+setInterval(setClock, 1000);
